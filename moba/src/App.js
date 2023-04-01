4f14bb266af9837e71657010ec55181567bbd85a
alszero <alszero05@gmail.com>
2023-04-01 23:55:25 +0900

    손 좀 봤음.. 근데 이게 맞아?

--- moba/src/App.js
+++ moba/src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          리액트 대체 어케 해야하죠... 좀 공부좀 하고 실행할께요...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>하이</h1>
+      </div>
     </div>
   );
 }
